3ce4199f5259e4b303660b2d35c783e134e8cda8
GrandpaNiuu <q3j1h8@gmail.com>
2026-06-16 06:35:27 +0800

    Cache self-check balanced practice release

--- sw.js
+++ sw.js
@@ -1,30 +1,29 @@
-const CACHE_NAME = 'english-reading-trainer-v11-exam-ui';
+const CACHE_NAME = 'english-reading-trainer-v12-selfcheck-balanced';
 const APP_ASSETS = [
   './',
   './index.html',
-  './css/style.css?v=exam-ui-20260616',
-  './css/mixed-questions.css?v=exam-ui-20260616',
-  './css/non-choice.css?v=exam-ui-20260616',
-  './css/polished-ui.css?v=exam-ui-20260616',
-  './js/passages.js?v=exam-ui-20260616',
-  './js/manual-passages.js?v=exam-ui-20260616',
-  './js/generated-passages.js?v=exam-ui-20260616',
-  './js/curated-passages.js?v=exam-ui-20260616',
-  './js/activate-generated-bank.js?v=exam-ui-20260616',
-  './js/advanced-question-types.js?v=exam-ui-20260616',
-  './js/exam-question-types.js?v=exam-ui-20260616',
-  './js/non-choice-question-types.js?v=exam-ui-20260616',
-  './js/app.js?v=exam-ui-20260616',
-  './js/question-type-labels.js?v=exam-ui-20260616',
-  './js/non-choice-runtime.js?v=exam-ui-20260616',
-  './manifest.json?v=exam-ui-20260616',
-  './icons/icon.svg?v=exam-ui-20260616'
+  './css/style.css?v=selfcheck-20260616',
+  './css/mixed-questions.css?v=selfcheck-20260616',
+  './css/non-choice.css?v=selfcheck-20260616',
+  './css/polished-ui.css?v=selfcheck-20260616',
+  './js/passages.js?v=selfcheck-20260616',
+  './js/manual-passages.js?v=selfcheck-20260616',
+  './js/generated-passages.js?v=selfcheck-20260616',
+  './js/curated-passages.js?v=selfcheck-20260616',
+  './js/activate-generated-bank.js?v=selfcheck-20260616',
+  './js/advanced-question-types.js?v=selfcheck-20260616',
+  './js/exam-question-types.js?v=selfcheck-20260616',
+  './js/non-choice-question-types.js?v=selfcheck-20260616',
+  './js/app.js?v=selfcheck-20260616',
+  './js/question-type-labels.js?v=selfcheck-20260616',
+  './js/non-choice-runtime.js?v=selfcheck-20260616',
+  './js/practice-mode-runtime.js?v=selfcheck-20260616',
+  './manifest.json?v=selfcheck-20260616',
+  './icons/icon.svg?v=selfcheck-20260616'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)));
   self.skipWaiting();
 });
 
@@ -40,12 +39,10 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match('./index.html'));
-    })
+    fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
   );
 });
