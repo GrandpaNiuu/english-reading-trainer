65f3bcca51c2e27dcd72db42c9d703c4ef443122
GrandpaNiuu <q3j1h8@gmail.com>
2026-06-16 05:55:20 +0800

    Include expanded manual sample bank

--- js/activate-generated-bank.js
+++ js/activate-generated-bank.js
@@ -1,3 +1,7 @@
+if (Array.isArray(window.MANUAL_PASSAGES)) {
+  window.PASSAGES = [...(window.PASSAGES || []), ...window.MANUAL_PASSAGES];
+}
+
 if (Array.isArray(window.GENERATED_PASSAGES)) {
   window.PASSAGES = [...(window.PASSAGES || []), ...window.GENERATED_PASSAGES];
 }
