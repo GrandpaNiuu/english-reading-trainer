4049a48746dea2a1cd6ee88beaab3d1a1bbbfa09
GrandpaNiuu <q3j1h8@gmail.com>
2026-06-16 05:28:44 +0800

    Include curated passage bank in activation

--- js/activate-generated-bank.js
+++ js/activate-generated-bank.js
@@ -1,3 +1,7 @@
 if (Array.isArray(window.GENERATED_PASSAGES)) {
   window.PASSAGES = [...(window.PASSAGES || []), ...window.GENERATED_PASSAGES];
 }
+
+if (Array.isArray(window.CURATED_PASSAGES)) {
+  window.PASSAGES = [...(window.PASSAGES || []), ...window.CURATED_PASSAGES];
+}
